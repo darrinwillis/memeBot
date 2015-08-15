effbd6bb8fd96b408ffce3a5d9f535897f257d34
Darrin Willis <willisdarrin@gmail.com>
2015-08-14 23:38:53 -0700

    Refactored RedditConnect into a singleton-style class

--- src/memebot/RedditConnect.java
+++ src/memebot/RedditConnect.java
@@ -2,6 +2,7 @@ package memebot;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
@@ -15,65 +16,76 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Key;
 
 public class RedditConnect {
-	
+
 	static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
 	static final JsonFactory JSON_FACTORY = new JacksonFactory();
-	
+
 	private static class RedditUrl extends GenericUrl {
-	    public RedditUrl(String encodedUrl) {
-	        super(encodedUrl);
-	        }
-	    public static RedditUrl topPosts(String subreddit) {
-	    	return new RedditUrl(
-	    			"https://reddit.com/r/" + subreddit + "/hot/.json");
-	    }
+		public RedditUrl(String encodedUrl) {
+			super(encodedUrl);
+		}
+		public static RedditUrl topPosts(String subreddit) {
+			return new RedditUrl(
+					"https://reddit.com/r/" + subreddit + "/hot/.json");
+		}
 	}
-	
+
 	public static class ReturnObject {
 		@Key("data")
 		public ReturnData returnData;
 	}
-	
+
 	public static class ReturnData {
 		@Key("children")
 		public List<PostWrap> postWraps;
 	}
-	
+
 	public static class PostWrap {
 		@Key("data")
-		public Post post;
+		public RedditPost post;
 	}
-	
-	public static class Post {
+
+	public static class RedditPost {
 		@Key
 		public String author;
-		
+
 		@Key
 		public String title;
-		
+
 		@Key
 		public String url;
 	}
-	
+
+	private HttpRequestFactory requestFactory;
+
+	public RedditConnect() {
+		requestFactory =
+				HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
+					@Override
+					public void initialize(HttpRequest request) {
+						request.setParser(new JsonObjectParser(JSON_FACTORY));
+					}
+				});
+	}
+
+	public List<RedditPost> getSubredditTop(String subreddit) throws IOException {
+
+		RedditUrl url = RedditUrl.topPosts(subreddit);
+		HttpRequest request = requestFactory.buildGetRequest(url);
+		// This could probably be generated more smartly, but hardcoded is fine
+		request.getHeaders().setUserAgent("windows:memeBot:v0.0.1 (by /u/xSherlock)");
+		ReturnObject retObj = request.execute().parseAs(ReturnObject.class);
+		return retObj.returnData.postWraps.stream().map(pw -> pw.post).collect(Collectors.toList());
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-	    HttpRequestFactory requestFactory =
-	            HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
-	                @Override
-	              public void initialize(HttpRequest request) {
-	                request.setParser(new JsonObjectParser(JSON_FACTORY));
-	              }
-	            });
-		
-		RedditUrl url = RedditUrl.topPosts("circlejerk");
-		
+		String subreddit = "askreddit";
+
+		RedditConnect rc = new RedditConnect();
 		try {
-			HttpRequest request = requestFactory.buildGetRequest(url);
-		    ReturnObject retObj = request.execute().parseAs(ReturnObject.class);
-		    for (PostWrap pw : retObj.returnData.postWraps) {
-		    	Post p = pw.post;
-		    	System.out.printf("[%s] %s \n    '%s'\n", p.author, p.title, p.url);
-		    }
+			rc.getSubredditTop(subreddit).stream().forEachOrdered(p ->
+			System.out.printf("[%s] %s \n    '%s'\n", p.author, p.title, p.url));
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
 		}
